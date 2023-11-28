eed309327c17ac7c53e3074cd132217ac1fb52da
msanand333 <shanmugan.murugesan@anywhere.co>
2023-11-29 00:02:29 +0530

    linting issue

--- app/components/search-bar/SearchBar.tsx
+++ app/components/search-bar/SearchBar.tsx
@@ -1,11 +1,11 @@
-'use client';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
-import React from 'react';
-import { SearchIcon } from '@chakra-ui/icons';
-import { SearchBarProps } from './types';
+"use client";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React from "react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { SearchBarProps } from "./types";
 
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-  const [keywords, setKeywords] = React.useState('');
+  const [keywords, setKeywords] = React.useState("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeywords(e.target.value);
@@ -20,21 +20,22 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   }, [keywords, onSearch]);
 
   return (
-    <InputGroup maxW='md' bg='#383733' borderRadius='4px'>
-      <InputLeftElement pointerEvents='none'>
-        <SearchIcon color='white' />
+    <InputGroup maxW="md" bg="#383733" borderRadius="4px">
+      <InputLeftElement pointerEvents="none">
+        <SearchIcon color="white" />
       </InputLeftElement>
       <Input
-        borderRadius='4px'
-        color='white'
-        borderColor='#797670'
-        borderWidth='2px'
-        boxSizing='border-box'
-        outline='none'
+        focusBorderColor="white"
+        borderRadius="4px"
+        color="white"
+        borderColor="#797670"
+        borderWidth="2px"
+        boxSizing="border-box"
+        outline="none"
         value={keywords}
         onChange={handleInputChange}
-        placeholder='Search here'
-        size='md'
+        placeholder="Search here"
+        size="md"
       />
     </InputGroup>
   );
