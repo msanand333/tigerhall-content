e188bfe79ac006bb68899e17ec2e4af08910fb06
msanand333 <shanmugan.murugesan@anywhere.co>
2023-11-28 07:07:52 +0530

    Added fonts and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,47 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontWeight: {
+      light: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+    },
+    fontSize: {
+      '3xs': ['8px', { lineHeight: '12px' }],
+      '2xs': ['10px', { lineHeight: '12px' }],
+      'xs': ['12px', { lineHeight: '14px' }],
+      'sm': ['14px', { lineHeight: '16px' }],
+      'md': ['16px', { lineHeight: '20px' }],
+      'lg': ['18px', { lineHeight: '22px' }],
+      'xl': ['20px', { lineHeight: '24px' }],
+      '2xl': ['24px', { lineHeight: '28px' }],
+      '3xl': ['28px', { lineHeight: '34px' }],
+      '4xl': ['32px', { lineHeight: '38px' }],
+      '5xl': ['36px', { lineHeight: '44px' }],
+      '6xl': ['40px', { lineHeight: '48px' }],
+    },
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        montreal: ['PP Neue Montreal', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          main: '#FF5900',
+          secondary: {
+            soSunny: '#f78104',
+            kindaYellow: '#faab36',
+            teal: '#005f60',
+            grey: '#f5f3ee',
+            black: '#000000',
+            white: '#ffffff',
+          },
+        },
       },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
