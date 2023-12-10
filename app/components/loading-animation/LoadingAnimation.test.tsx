3e75f8308dfd675c1dcd1dacd5941fef64749ea5
msanand333 <shanmugan.murugesan@anywhere.co>
2023-12-10 22:41:59 +0530

    Removing comments

--- app/components/loading-animation/LoadingAnimation.test.tsx
+++ app/components/loading-animation/LoadingAnimation.test.tsx
@@ -1,4 +1,3 @@
-// LoadingAnimation.test.tsx
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // For additional matchers
@@ -7,10 +6,7 @@ import { LoadingAnimation } from "./LoadingAnimation";
 describe("LoadingAnimation", () => {
   it("renders the LoadingAnimation component", () => {
     const { container } = render(<LoadingAnimation />);
-    // Ensure the component renders without errors
-    expect(container).toBeInTheDocument();
 
-    // You can also make specific assertions about the rendered HTML, for example:
-    // expect(container.firstChild).toHaveClass('loader');
+    expect(container).toBeInTheDocument();
   });
 });
